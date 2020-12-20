d419b674537428cdc1ac0d4d4f007a88699de04d
EgorAlekseev <54812985+EgorAlekseev@users.noreply.github.com>
2020-12-21 01:17:38 +0300

    Add files

--- Labs/Lab_8/Lab_8_Control_Task_1/Lab_8_Control_Task_1/Lab_8_Control_Task_1.cpp
+++ Labs/Lab_8/Lab_8_Control_Task_1/Lab_8_Control_Task_1/Lab_8_Control_Task_1.cpp
@@ -17,13 +17,109 @@ using namespace std;
 
 class Time
 {
+private:
     int hours;
     int minutes;
     int seconds;
+
+public:
+    Time()
+    {
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+    }
+  
+
+    Time(int h, int m, int s)
+    {
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+
+        if (s > 60)
+        {
+            minutes = minutes + (floor(s / 60));
+            seconds = s % 60;
+        }
+        else
+        {
+            seconds = s;
+        }
+
+        if (m > 60)
+        {
+            hours = floor(m / 60);
+            minutes = minutes + (m % 60);
+        }
+        else
+        {
+            minutes = minutes + m;
+        }
+
+        if (h > 24)
+        {
+            hours = hours + (floor(h % 24));
+        }
+        else
+        {
+            hours = hours + h;
+        }
+    }
+
+    void add_time(int h, int m, int s)
+    {
+        hours = hours + h;
+        minutes = minutes + m;
+        seconds = seconds + s;
+
+        if (seconds > 60)
+        {
+            minutes = minutes + (floor(s / 60));
+            seconds = seconds % 60;
+        }
+
+        if (minutes > 60)
+        {
+            hours = hours + floor(m / 60);
+            minutes = minutes % 60;
+        }
+        
+        if (hours > 24)
+        {
+            hours = hours + (floor(hours % 24));
+        }
+       
+    }
+
+    void Print()
+    {
+        cout << hours << ":" << minutes << ":" << seconds << endl;
+    }
+
+    int getHours() { return hours; }
+    int getMinutes() { return minutes; }
+    int getSeconds() { return seconds; }
+
+    void setHours(int h) { hours = h; }
+    void setMinutes(int m) { minutes = m; }
+    void setSeconds(int s) { seconds = s; }
 };
 
 int main()
 {
+    system("chcp 1251");
+    Time s(1,70,70);
     
+    Time d(2, 80, 80);
+   
+    Time m;
+    m.setHours(s.getHours() + d.getHours());
+    m.setMinutes(s.getMinutes() + d.getMinutes());
+    m.setSeconds(s.getSeconds() + d.getSeconds());
+
+    m.Print();
+   
+    return 0;
 }
 
