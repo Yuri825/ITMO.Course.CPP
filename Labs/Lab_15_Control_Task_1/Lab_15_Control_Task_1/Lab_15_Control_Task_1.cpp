19c851bcc11a5098f547ce2814a1a5ce891d0c45
EgorAlekseev <54812985+EgorAlekseev@users.noreply.github.com>
2020-12-24 22:19:32 +0300

    Add files

---
+++ Labs/Lab_15_Control_Task_1/Lab_15_Control_Task_1/Lab_15_Control_Task_1.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream> 
+#include <windows.h>
+#include <map> 
+#include <string> 
+using namespace std;
+
+
+void readFind(); // Чтение 
+void writeFind(); // Ввод
+int action;
+
+map<string, int> directory;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	directory["Иванов"] = 3;
+	directory["Петров"] = 4;
+	directory["Смирнов"] = 2;
+
+	cout << "для получения данных введите: 1" << endl;
+	cout << "Для ввода данных введите 2" << endl;
+	cin >> action;
+
+	while (true)
+	{
+		if (action == 1)
+		{
+			readFind();
+		}
+
+		if (action == 2)
+		{
+			writeFind();
+		}
+		else
+		{
+			cout << "Вы веели не то число. Введите 1 - для получения данных, 2 - для записи" << endl;
+			cin >> action;
+		}
+	}
+		
+	return 0;
+}
+
+void readFind()
+{
+	string name;
+
+		cout << "Введите фамилию студента\n";
+		cin >> name;
+
+		if (directory.find(name) != directory.end())
+		{
+			cout << "Оценка " << name << " - " << directory[name] << "\n";
+		}
+		else
+		{
+			cout << "Извините, в каталоге нет " << name << "\n";
+		}
+
+		cout << "для получения данных введите: 1" << endl;
+		cout << "Для ввода данных введите 2" << endl;
+		cin >> action;
+			
+}
+
+void writeFind()
+{
+	string name;
+	int grade;
+	cout << "Введите фамилию абонента\n";
+	cin >> name;
+
+		if (directory.find(name) == directory.end())
+		{
+			cout << "Введите оценку" << endl;
+			cin >> grade;
+			directory[name] = grade;
+		}
+		else
+		{
+			cout << "Такая фамилия уже есть"<< "\n";
+		}
+
+	cout << "для получения данных введите: 1" << endl;
+	cout << "Для ввода данных введите 2" << endl;
+	cin >> action;
+}
+		
+		
+			
+
+
