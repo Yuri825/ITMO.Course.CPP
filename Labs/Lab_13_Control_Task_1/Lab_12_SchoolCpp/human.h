601c986e79c9c17df60ea3d5c7492f3bcf8b24af
EgorAlekseev <54812985+EgorAlekseev@users.noreply.github.com>
2021-01-06 13:14:24 +0300

    Add files

---
+++ Labs/Lab_13_Control_Task_1/Lab_12_SchoolCpp/human.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class human
+{
+public:
+	// Конструктор класса human
+	human(string last_name, string name, string second_name)
+	{
+		this->last_name = last_name;
+		this->name = name;
+		this->second_name = second_name;
+	}
+	// Получение ФИО человека
+	string get_full_name()
+	{
+		ostringstream full_name;
+		full_name << this->last_name << " "
+			<< this->name << " "
+			<< this->second_name;
+		return full_name.str();
+	}
+
+	virtual void getInfo(){} 
+
+private:
+	string name; // имя
+	string last_name; // фамилия
+	string second_name; // отчество
+};
+
+
---
+++ Labs/Lab_13_Control_Task_1/Lab_12_SchoolCpp/teacher.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "human.h"
+#include <string>
+
+using namespace std;
+
+class teacher : public human {
+	// Конструктор класса teacher
+public:
+	teacher(
+		string last_name,
+		string name,
+		string second_name,
+		// Количество учебных часов за семестр у преподавателя
+		unsigned int work_time
+	) : human(
+		last_name,
+		name,
+		second_name
+	) {
+		this->work_time = work_time;
+	}
+
+	// Получение количества учебных часов
+	unsigned int get_work_time()
+	{
+		return this->work_time;
+	}
+
+	void getInfo()
+	{
+		cout << "\nИнформация о преподавателе:" << endl;
+	}
+private:
+	// Учебные часы
+	unsigned int work_time;
+};
